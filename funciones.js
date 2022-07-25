9601225820f2c30b7d38bc7f309fb097b86631ab
Luis Meza <luisjosemeza90@gmail.com>
2022-07-25 17:59:49 +0200

    commit  inicio descripcion
    - creacion de la seccion descripcion
    - desarrollo de logo en funciones.js

--- funciones.js
+++ funciones.js
@@ -1,1 +1,7 @@
 console.log("funciones cargadas");
+
+const text = document.querySelector('.text-imagen p');
+text.innerHTML = text.innerText.split("").map(
+    (char, i) =>
+    `<span style="transform:rotate(${i * 8.9}deg)">${char}</span>`
+).join("")
