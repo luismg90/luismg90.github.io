51670c641e933a03b8e712299bc3d59272e920cd
Luis Meza <luisjosemeza90@gmail.com>
2022-07-25 22:16:55 +0200

    commit cambio en seccion descripcion
    - cambio de estilo de la seccion descripcion.
    - Rueda con foto y texto giratorio.
    -funciones.js para agregar animacion a la barra de skills.

--- funciones.js
+++ funciones.js
@@ -3,5 +3,20 @@ console.log("funciones cargadas");
 const text = document.querySelector('.text-imagen p');
 text.innerHTML = text.innerText.split("").map(
     (char, i) =>
-    `<span style="transform:rotate(${i * 8.9}deg)">${char}</span>`
+    `<span style="transform:rotate(${i * 8}deg)">${char}</span>`
 ).join("")
+
+$('.skill-per').each(function(){
+    var $this = $(this);
+    var per = $this.attr('per');
+    $this.css("width",per+'%');
+    $({animatedValue: 0}).animate({animatedValue: per},{
+        duration: 1000,
+        step: function(){
+            $this.attr('per', Math.floor(this.animatedValue) + '%');
+        },
+        complete: function(){
+            $this.attr('per', Math.floor(this.animatedValue) + '%');
+        }
+    });
+});
