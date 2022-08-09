4045263d11f50b6b0a287c3e6ba34adfe0b639e5
Luis Meza <luisjosemeza90@gmail.com>
2022-08-09 20:11:13 +0200

    commit preFin de proyecto
    update en todas las secciones.
    fin de los medias queries, 1200, 992, 768, 576 y 350 px.
    anadimos la funcion de enviar email-pero de momento no funciona. tocara ver eso manana.

--- funciones.js
+++ funciones.js
@@ -35,12 +35,30 @@ $(document).ready(function(){
 
 // Funcion para recibir correos de email.js
 
-
-// Funcion para el scroll de los elementos con transiciones
-
-const scrollElements = document.querySelectorAll(".js-scroll");
-
-
+window.onload = function(){
+    // Window cargado (DOM)
+    // $("#contact-form")
+    document.getElementById("f-campos").addEventListener('boton-contacto', function( event ){
+        event.preventDefault();
+        
+        emailjs.sendForm('service_9kn4iub', 'template_9atewqs', this)
+        .then( 
+            function( success ){
+                // Envío de correo con éxito
+                console.log("EXITO")
+                console.log(success)
+            $("input[type=text]").val("");
+            $("textarea").val("");
+              $("f-campos").append("<div>Correo enviado con éxito </div>")
+            }, 
+            function(error){
+                // ERROR!
+                console.log("ERROR")
+                console.log(error)
+            }
+        );
+    });
+};
 
 
 $(window).scroll(function(){
